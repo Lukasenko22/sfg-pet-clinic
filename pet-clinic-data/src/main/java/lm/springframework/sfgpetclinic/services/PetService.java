6ec72cb38bd49fe95c838ca5178dbed081021eb1
Lukas Molcan <lukas.molcan@gmail.com>
2020-06-07 17:16:56 +0200

    Created Common Interface. Closes #11

--- pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/services/PetService.java
@@ -2,11 +2,6 @@ package lm.springframework.sfgpetclinic.services;
 
 import lm.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package lm.springframework.sfgpetclinic.services;
 
 import lm.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
