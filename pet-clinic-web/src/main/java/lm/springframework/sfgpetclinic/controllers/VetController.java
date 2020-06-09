29569e61aa98ac2c73a8142cd8e780839f7f8efd
Lukas Molcan <lukas.molcan@gmail.com>
2020-06-09 21:10:06 +0200

    Showed vets on index page. Closes #15

--- pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package lm.springframework.sfgpetclinic.controllers;
 
+import lm.springframework.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
