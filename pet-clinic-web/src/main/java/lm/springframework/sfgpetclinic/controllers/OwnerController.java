8c443ffa9b578f5fff05c841b18ad95cb9ec5f8d
Lukas Molcan <lukas.molcan@gmail.com>
2020-06-09 21:06:26 +0200

    Showed owners on index page. Closes #16

--- pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package lm.springframework.sfgpetclinic.controllers;
 
+import lm.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOnwers(){
+    public String listOnwers(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
