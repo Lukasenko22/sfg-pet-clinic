a56ce894b4da8816df95cdc66acfc8807bff599d
Lukas Molcan <lukas.molcan@gmail.com>
2020-06-13 17:21:07 +0200

    Menu links fixed, closes #35

--- pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -22,4 +22,9 @@ public class OwnerController {
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    private String findOwners(){
+        return "notimplemented";
+    }
 }
