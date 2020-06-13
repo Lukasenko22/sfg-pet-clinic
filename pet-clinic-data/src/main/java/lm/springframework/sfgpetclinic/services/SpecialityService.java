cc0373d02cde8e736a37d418e5376c31d27c49eb
Lukas Molcan <lukas.molcan@gmail.com>
2020-06-13 16:31:24 +0200

    Add Speciality Map Service Impl, closes #31

---
+++ pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package lm.springframework.sfgpetclinic.services;
+
+import lm.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
