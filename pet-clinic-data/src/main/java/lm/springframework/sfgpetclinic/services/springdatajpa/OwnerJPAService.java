01fc6d657b23238cacac4dcf4eca0ec4360cdbc2
Lukas Molcan <lukas.molcan@gmail.com>
2020-06-14 18:38:47 +0200

    Repository for Vet implemented, closes #41

--- pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/services/springdatajpa/OwnerJPAService.java
+++ pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/services/springdatajpa/OwnerJPAService.java
@@ -5,6 +5,7 @@ import lm.springframework.sfgpetclinic.repositories.OwnerRepository;
 import lm.springframework.sfgpetclinic.repositories.PetRepository;
 import lm.springframework.sfgpetclinic.repositories.PetTypeRepository;
 import lm.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,7 @@ public class OwnerJPAService implements OwnerService {
     private final PetRepository petRepository;
     private final PetTypeRepository petTypeRepository;
 
+    @Autowired
     public OwnerJPAService(OwnerRepository ownerRepository,
                            PetRepository petRepository,
                            PetTypeRepository petTypeRepository) {
