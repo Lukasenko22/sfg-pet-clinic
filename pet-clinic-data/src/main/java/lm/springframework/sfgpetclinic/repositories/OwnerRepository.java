232c0e0690c9ce7fc3526fc85f0d5f07859ec59a
Lukas Molcan <lukas.molcan@gmail.com>
2020-06-14 18:34:00 +0200

    Repository for Owner implemented, closes #40

--- pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/lm/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,5 +3,8 @@ package lm.springframework.sfgpetclinic.repositories;
 import lm.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+    Set<Owner> findByLastName(String lastName);
 }
