19f0f3f29786cbd02438972409f2a6c44de89b20
Lukas Molcan <lukas.molcan@gmail.com>
2020-10-18 16:14:22 +0200

    Implemented JSON Vet List api and updated vets/index.html, Closes #63

--- pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/lm/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package lm.springframework.sfgpetclinic.controllers;
 
+import lm.springframework.sfgpetclinic.model.Vet;
 import lm.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping({"/vets","/vets.html"})
@@ -21,5 +26,10 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
